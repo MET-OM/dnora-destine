7fd241757153bc7317a1709560ff4800daf638ef
Jan-Victor Björkqvist <janvb@met.no>
2025-09-09 13:00:03 +0200

    ice reader

--- dnora_destine/polytope_functions.py
+++ dnora_destine/polytope_functions.py
@@ -142,7 +142,7 @@ def download_ecmwf_wave_from_destine(start_time, filename: str, end_time=None) -
     c.retrieve("destination-earth", request_waves, filename)
 
 
-def destine_wave_ds_read(
+def ds_wave_polytope_read(
     start_time: pd.Timestamp,
     end_time: pd.Timestamp,
     url: str,
@@ -185,3 +185,92 @@ def destine_wave_ds_read(
     obj.set_spec(Ed)
 
     return obj.ds()
+
+
+def download_ecmwf_ice_from_destine(start_time, end_time, lon, lat, folder: str) -> str:
+    """Downloads DestinE ClimateDT data portfolio data from the Destine Earth Store System for a
+    given area and time period"""
+    start_time = pd.Timestamp(start_time)
+    end_time = pd.Timestamp(end_time)
+    try:
+        from polytope.api import Client
+    except ImportError as e:
+        msg.advice(
+            "The polytope package is required to acces these data! Install by e.g. 'python -m pip install polytope-client' and 'conda install cfgrib eccodes=2.41.0'"
+        )
+        raise e
+    c = Client(address="polytope.lumi.apps.dte.destination-earth.eu")
+
+    filename = f"{folder}/ECMWF_temp_ice.grib"  # Switch to this in production. Then the files will be cleaned out
+    # filename = f"{folder}/destine_temp.grib"
+    request_ice = {
+        "class": "d1",
+        "expver": "0001",
+        "dataset": "extremes-dt",
+        "stream": "oper",
+        "type": "fc",
+        "levtype": "sfc",
+        "param": "31",
+        "time": "00",
+        "step": "0/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23",
+        "area": [
+            int(np.ceil(lat[1])),
+            int(np.floor(lon[0])),
+            int(np.floor(lat[0])),
+            int(np.ceil(lon[1])),
+        ],
+    }
+    date_str = start_time.strftime("%Y%m%d")
+    request_ice["date"] = date_str
+
+    c.retrieve("destination-earth", request_ice, filename)
+    return filename
+
+
+def ds_ice_polytope_read(
+    start_time: pd.Timestamp,
+    end_time: pd.Timestamp,
+    url: str,
+    lon: np.ndarray,
+    lat: np.ndarray,
+    dnora_class,
+    **kwargs,
+):
+
+    grib_file = download_ecmwf_ice_from_destine(
+        start_time, end_time, lon=lon, lat=lat, folder="dnora_ice_temp"
+    )
+
+    ds = xr.open_dataset(grib_file, engine="cfgrib", decode_timedelta=True)
+    lons, lats, sic = (
+        ds.siconc.longitude.values,
+        ds.siconc.latitude.values,
+        ds.siconc.values,
+        # ds.sit.values,
+    )
+    native_dlon, native_dlat = 1 / 8, 1 / 30
+    xi = np.arange(min(lons), max(lons), native_dlon)
+    yi = np.arange(min(lats), max(lats), native_dlat)
+    Xi, Yi = np.meshgrid(xi, yi)
+
+    Nt = len(ds.step)
+    sici = np.zeros((Nt, len(yi), len(xi)))
+    # siti = np.zeros((Nt, len(yi), len(xi)))
+    # If this becomes slow, we need to think about 3D interpolation / resuing weights
+    for n in range(Nt):
+        sici[n, :, :] = griddata(
+            list(zip(lons, lats)), sic[n, :], (Xi, Yi), method="nearest"
+        )
+        # siti[n, :, :] = griddata(
+        #    list(zip(lons, lats)), sit[n, :], (Xi, Yi), method="nearest"
+        # )
+
+    data = dnora_class(lon=xi, lat=yi, time=ds.time + ds.step)
+    data.set_sic(sici)
+    # data.set_sit(siti)
+    lo, la = utils.grid.expand_area(
+        lon, lat, expansion_factor=1, dlon=native_dlon, dlat=native_dlat
+    )
+    data = data.sel(lon=slice(*lo), lat=slice(*la))
+
+    return data.sel(time=slice(start_time, end_time)).ds()
